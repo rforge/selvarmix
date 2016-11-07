42a3d6201ee8d9338325f4e16c0f5dc5753adeb8
masedki <masedki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-07 10:25:10 +0000

    correction des deux remarques issues du dépot sur cran.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/selvarmix@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/orderlikL.R
+++ pkg/R/orderlikL.R
@@ -1,6 +1,6 @@
 orderlikL<- function(x, z, nbcores)
 {
-  
+  g <- length(unique(z))
   loglikwrapper <-function(j,x,g){return(mixmodLearn(x[,j], knownLabels=z, models=mixmodGaussianModel(family = "all"))@bestResult@likelihood)}
   return(order(simplify2array(mclapply(X=1:ncol(x), 
                                        FUN = loglikwrapper, 
